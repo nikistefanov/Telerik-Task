003088995a1f81c06738e600163953b9f4513349
nikistefanov <nikistefanovbg89@gmail.com>
2016-03-09 01:25:47 +0200

    some changes for popup search bar styles

--- js/events/popupInputEvents.js
+++ js/events/popupInputEvents.js
@@ -1,11 +1,11 @@
 var popupInputEvents = (function() {
   function load($container) {
-    $container.on('focusin', '#search-bar', function() {
-      $(this).closest('.popup__search').addClass('search--focused');
+    $container.on('focusin', '#popup-search-bar', function() {
+      $(this).closest('.popup__search').addClass('popup__search__bar--focused');
     });
 
-    $container.on('focusout', '#search-bar', function() {
-      $(this).closest('.popup__search').removeClass('search--focused');
+    $container.on('focusout', '#popup-search-bar', function() {
+      $(this).closest('.popup__search').removeClass('popup__search__bar--focused');
     });
 
     $container.on('click', '.popup__checkbox', function() {
