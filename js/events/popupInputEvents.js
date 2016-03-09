e0ac028a762f769381f8cc5f349f60dc1add2307
nikistefanov <nikistefanovbg89@gmail.com>
2016-03-09 11:46:48 +0200

    Removed unused bower components. Added a clear button in the search input. Starts with Unit testing...

--- js/events/popupInputEvents.js
+++ js/events/popupInputEvents.js
@@ -2,10 +2,12 @@ var popupInputEvents = (function() {
   function load($container) {
     $container.on('focusin', '#popup-search-bar', function() {
       $(this).closest('.popup__search').addClass('popup__search__bar--focused');
+      $('#popup-button-clear').show();
     });
 
     $container.on('focusout', '#popup-search-bar', function() {
       $(this).closest('.popup__search').removeClass('popup__search__bar--focused');
+      $('#popup-button-clear').hide();
     });
 
     $container.on('click', '.popup__checkbox', function() {
